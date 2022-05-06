fee6b0792b20875406ed9facb6981cafaab57e91
Jochen Schäfer <jochen.schaefer@uni-mannheim.de>
2022-05-06 10:23:04 +0200

    Added String Parsing for XMRPool

--- ParseUtils.R
+++ ParseUtils.R
@@ -28,6 +28,22 @@ process_paymentstring <- function(paymentstring, skip=0){
   return(payments)
 }
 
+process_paymentstring_xmrpool <- function(paymentstring, skip=0){
+  paymentDates <- paymentstring[seq(2,length(paymentstring),2)]
+  payments <- paymentstring[seq(1,length(paymentstring),2)]
+  
+  payments <- strsplit(payments, ":")
+  payments <- lapply(payments, function(x) data.frame(t(x), stringsAsFactors = FALSE))
+  payments <- bind_rows(payments)
+  if(skip>0){
+    payments <- payments[,-c(1:skip)]
+  }
+  names(payments) <- c("TxHash", "Amount", "UNK", "Mixin")
+  payments$Date <- paymentDates
+  return(payments)
+}
+
+
 process_paymentstring_minexmr <- function(paymentstring, skip=0){
   payments <- strsplit(paymentstring, ":")
   payments <- lapply(payments, function(x) data.frame(t(x), stringsAsFactors = FALSE))
